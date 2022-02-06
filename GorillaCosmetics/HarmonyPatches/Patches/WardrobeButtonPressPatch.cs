a00912bc80b9348f147bfa8c41097c98a493aebb
Graicc <33105645+Graicc@users.noreply.github.com>
2022-02-05 22:47:20 -0500

    Fix bug with trying on hats

--- GorillaCosmetics/HarmonyPatches/Patches/WardrobeButtonPressPatch.cs
+++ GorillaCosmetics/HarmonyPatches/Patches/WardrobeButtonPressPatch.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using GorillaNetworking;
+using GorillaCosmetics.Utils;
 
 namespace GorillaCosmetics.HarmonyPatches.Patches
 {
@@ -12,10 +13,26 @@ namespace GorillaCosmetics.HarmonyPatches.Patches
 	{
 		internal static void Postfix(CosmeticsController __instance, CosmeticsController.CosmeticItem cosmeticItem)
 		{
-			if (Plugin.SelectionManager.CurrentHat != null && cosmeticItem.itemSlot == "hat" && cosmeticItem.itemName != __instance.nullItem.itemName)
+			if (CosmeticItemUtils.ContainsHat(cosmeticItem))
 			{
 				Plugin.SelectionManager.ResetHat();
 			}
 		}
 	}
+
+	[HarmonyPatch(typeof(CosmeticsController))]
+	[HarmonyPatch("PressFittingRoomButton", MethodType.Normal)]
+	internal class FittingRoomButtonPressPatch
+	{
+		internal static void Postfix(CosmeticsController __instance, FittingRoomButton pressedFittingRoomButton)
+		{
+			if (CosmeticItemUtils.ContainsHat(pressedFittingRoomButton.currentCosmeticItem))
+			{
+				Plugin.SelectionManager.ResetHat();
+			}
+		}
+	}
+
+	// This doesn't account for purchasing cosmetics with shiny rocks.
+	// That is too small of an edge case to bother dealing with.
 }
